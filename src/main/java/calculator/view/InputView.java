8e01658df39cd95f37361e354938819be26dc896
woohyeon <woohyeon6885@gmail.com>
2025-10-16 02:00:39 +0900

    feat: 등록되지 않은 구분자 검증 기능 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -7,7 +7,41 @@ import java.util.List;
 
 public class InputView {
 
+    private String delimiters;
+
+    public InputView() {
+        delimiters = "[,:]";
+    }
+
+
     private boolean isStartWithDoubleSlash(String input) {
         return input.startsWith("//");
     }
+
+    private void validateUnsupportedDelimiter(String input) {
+        char[] inputChars = input.toCharArray();
+
+        for (char inputChar : inputChars) {
+            if (isDigit(inputChar)) continue;
+            if (inputChar == '.') continue;
+
+            boolean isProper = false;
+            for (int i = 1; i < delimiters.length() - 1; i ++) {
+                if (inputChar == delimiters.charAt(i)) {
+                    isProper = true;
+                    break;
+                }
+            }
+
+            if (!isProper) {
+                throw new IllegalArgumentException("잘못된 구분자가 사용되었습니다: " + inputChar);
+            }
+
+        }
+    }
+
+    private boolean isDigit(char target) {
+        return (target >= 48 && target <= 57);
+    }
+
 }
