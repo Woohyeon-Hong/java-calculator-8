c24b33bc9780aa62fa67cbc238a72d99dbd416c5
woohyeon <woohyeon6885@gmail.com>
2025-10-16 23:32:41 +0900

    feat: 추출된 숫자를 연산하는 기능 구현

---
+++ src/main/java/calculator/CalculatorController.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import calculator.model.Numbers;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CalculatorController() {
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
+
+    public void calculate() {
+        outputView.printCalculatorStartPrompt();
+        Numbers numbers = inputView.readInputString();
+        numbers.addNumberListValues();
+    }
+}
--- src/main/java/calculator/model/Numbers.java
+++ src/main/java/calculator/model/Numbers.java
@@ -6,12 +6,24 @@ import java.util.List;
 public class Numbers {
 
     private final List<Double> numberList;
+    private double sum;
 
     public Numbers(List<Double> numberList) {
         this.numberList = new ArrayList<>(numberList);
+        sum = 0.0;
     }
 
     public List<Double> getNumberList() {
         return new ArrayList<>(numberList);
+    }
+
+    public void addNumberListValues() {
+        for (double value : numberList) {
+            sum += value;
+        }
+    }
+
+    public double getSum() {
+        return sum;
     }
 }
